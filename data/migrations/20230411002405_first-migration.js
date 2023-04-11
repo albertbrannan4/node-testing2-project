86223ae88f5f1ebf3a3a816c09cc5836209ca8bb
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-10 20:51:30 -0400

    update migration files

--- data/migrations/20230411002405_first-migration.js
+++ data/migrations/20230411002405_first-migration.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("user", (table) => {
+    table.increments("id");
+    table.string("username").notNullable();
+    table.string("password").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropIfTableExists("user");
 };
--- knexfile.js
+++ knexfile.js
@@ -24,8 +24,7 @@ module.exports = {
       password: "password",
     },
     pool: {
-      min: 2,
-      max: 10,
+      afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
     },
     migrations: {
       tableName: "knex_migrations",
