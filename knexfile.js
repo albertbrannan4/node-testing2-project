cfde732cfffa175b3ab6db1d47fdebbb6ce71899
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-12 11:21:22 -0400

    added common to knex file

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,28 @@
 // Update with your config settings.
-
-module.exports = {
+const common = {
   useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+  },
+  client: "sqlite3",
+  migrations: {
+    tableName: "knex_migrations",
+  },
+};
+module.exports = {
   development: {
-    client: "sqlite3",
+    ...common,
     connection: {
       filename: "./data/user.db3",
     },
   },
 
   testing: {
-    client: "sqlite3",
+    ...common,
     connection: {
       filename: "./data/test.db3",
     },
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
-
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-    },
-  },
+  production: {},
 };
