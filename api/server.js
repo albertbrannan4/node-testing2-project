8277aba143097ba52b497f69a357138a8f5a55a2
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-12 11:02:44 -0400

    created user router

--- api/server.js
+++ api/server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const UserRouter = require("./User/user-router");
+
 const server = express();
 
+server.use(express.json());
+server.use("/user", UserRouter);
 module.exports = server;
